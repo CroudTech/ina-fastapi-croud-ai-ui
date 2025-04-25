fd4127a26efa1803e7bebd4e291698d1abe73fc1
Sam Green <sam.green@croud.com>
2025-04-25 21:33:37 +0100

    feat: remove help

--- src/lib/components/layout/Help.svelte
+++ src/lib/components/layout/Help.svelte
@@ -11,30 +11,6 @@
 </script>
 
 <div class=" hidden lg:flex fixed bottom-0 right-0 px-1 py-1 z-20">
-	<button
-		id="show-shortcuts-button"
-		class="hidden"
-		on:click={() => {
-			showShortcuts = !showShortcuts;
-		}}
-	/>
-
-	<HelpMenu
-		showDocsHandler={() => {
-			showShortcuts = !showShortcuts;
-		}}
-		showShortcutsHandler={() => {
-			showShortcuts = !showShortcuts;
-		}}
-	>
-		<Tooltip content={$i18n.t('Help')} placement="left">
-			<button
-				class="text-gray-600 dark:text-gray-300 bg-gray-300/20 size-4 flex items-center justify-center text-[0.7rem] rounded-full"
-			>
-				?
-			</button>
-		</Tooltip>
-	</HelpMenu>
 </div>
 
 <ShortcutsModal bind:show={showShortcuts} />
